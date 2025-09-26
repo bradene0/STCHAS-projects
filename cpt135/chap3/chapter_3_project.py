3ca532e2ae738b0d0f4f2466eb99b0f5cc2a9da3
bradene0 <bradene0@outlook.com>
2025-09-26 10:27:05 -0500

    fixed bugs

--- cpt135/chap3/chapter_3_project.py
+++ cpt135/chap3/chapter_3_project.py
@@ -4,12 +4,12 @@ import math
 COVERAGE_PER_GALLON = 350
 
 paint_price = {
-    "blue" = 50,
-    "orange" = 30,
-    "purple" = 5.99,
-    "gray" = 12.99,
-    "tan" = 20,
-    "white" = 25,
+    "blue": 50,
+    "orange": 25,
+    "purple": 30,
+    "gray": 12.99,
+    "tan": 20,
+    "white": 25
 }
 
 
@@ -17,14 +17,14 @@ wall_length = float(input("Please enter the wall length in feet"))
 ceiling_height = float(input("Please enter the ceiling height in feet"))
 color_choice  =  input("Please enter the color you wish to use, (blue, orange, purple, gray, tan, white")
 wall_area = ceiling_height * wall_length
-wall_area =roung(wall_area)
+wall_area =round(wall_area)
 
 paint_needed = wall_area / COVERAGE_PER_GALLON
 paint_needed = round(paint_needed, 2)
 cans_needed = math.ceil(paint_needed)
 
 if color_choice in paint_price:
-    total_cost = paint_price{color_choice} * cans_needed
+    total_cost = paint_price[color_choice] * cans_needed
     print(f"Using {color_choice} paint will cost: ${round(total_cost)}")
 else:
     print("Please enter a color from the list provided")
