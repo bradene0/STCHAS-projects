155fa3c2f32da8e17249f32bf8cd1170250635a2
bradene0 <bradene0@outlook.com>
2025-09-26 10:24:00 -0500

    full logic

--- cpt135/chap3/chapter_3_project.py
+++ cpt135/chap3/chapter_3_project.py
@@ -12,7 +12,22 @@ paint_price = {
     "white" = 25,
 }
 
+
 wall_length = float(input("Please enter the wall length in feet"))
 ceiling_height = float(input("Please enter the ceiling height in feet"))
+color_choice  =  input("Please enter the color you wish to use, (blue, orange, purple, gray, tan, white")
 wall_area = ceiling_height * wall_length
 wall_area =roung(wall_area)
+
+paint_needed = wall_area / COVERAGE_PER_GALLON
+paint_needed = round(paint_needed, 2)
+cans_needed = math.ceil(paint_needed)
+
+if color_choice in paint_price:
+    total_cost = paint_price{color_choice} * cans_needed
+    print(f"Using {color_choice} paint will cost: ${round(total_cost)}")
+else:
+    print("Please enter a color from the list provided")
+
+
+
