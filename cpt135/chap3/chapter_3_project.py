5fec96ace33869a9cd27be7ed1cde6748ca7b9c5
bradene0 <bradene0@outlook.com>
2025-09-26 10:18:46 -0500

    input and calculations

--- cpt135/chap3/chapter_3_project.py
+++ cpt135/chap3/chapter_3_project.py
@@ -1,1 +1,18 @@
 # Braden Evans
+import math 
+
+COVERAGE_PER_GALLON = 350
+
+paint_price = {
+    "blue" = 50,
+    "orange" = 30,
+    "purple" = 5.99,
+    "gray" = 12.99,
+    "tan" = 20,
+    "white" = 25,
+}
+
+wall_length = float(input("Please enter the wall length in feet"))
+ceiling_height = float(input("Please enter the ceiling height in feet"))
+wall_area = ceiling_height * wall_length
+wall_area =roung(wall_area)
