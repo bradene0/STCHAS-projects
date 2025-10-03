adda058cc198a43ae4f2ee6e80568c8b6c36ee94
bradene0 <bradene0@outlook.com>
2025-10-03 08:25:10 -0500

    apply best discount and compute total

--- cpt135/chap4/chapter_4_project.py
+++ cpt135/chap4/chapter_4_project.py
@@ -29,5 +29,17 @@ is_student = get_yes_no("Are you currently a student? Y/N: ")
 
 subtotal = round(tickets * TICKET_PRICE, 2)
 eligible_discounts = []
+if is_student:
+    eligible_discounts.append(("Student", STUDENT_DISCOUNT))
+if is_senior:
+    eligible_discounts.append(("Senior", SENIOR_DISCOUNT))
+
+if eligible_discounts:
+    discount_name, discount_rate = max(eligible_discounts, key=lambda x: x[1]) # my favorite concise selector(max by rate)
+    discount_amount = round(subtotal * discount_rate, 2)
+else:
+    discount_name, discount_rate, discount_amount = None, 0.0, 0.0
+
+total = round(subtotal - discount_amount, 2)
 
 
