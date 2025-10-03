d09953c4db5d5d8b514bba017e680b940c55e550
bradene0 <bradene0@outlook.com>
2025-10-03 08:33:01 -0500

    cleanup and finish

--- cpt135/chap4/chapter_4_project.py
+++ cpt135/chap4/chapter_4_project.py
@@ -4,16 +4,13 @@ SENIOR_DISCOUNT = .10
 STUDENT_DISCOUNT = .20
 
 def get_ticket_count():
-    print("Welcome to my Movie Theatre!")
+    print("Welcome to Braden's Movie Theatre!")
     while True:
-        raw = input("Tickets are 10$ each, please enter how many you would like: ").strip()
+        raw = input("Tickets are 10$ each, please enter how many you would like(1-99): ").strip()
         if raw.isdigit():
             n = int(raw)
             if 1 <=n <=99:
                 return n 
-
-
-
 
 def get_yes_no(prompt):
     while True:
@@ -38,8 +35,16 @@ if eligible_discounts:
     discount_name, discount_rate = max(eligible_discounts, key=lambda x: x[1]) # my favorite concise selector(max by rate)
     discount_amount = round(subtotal * discount_rate, 2)
 else:
-    discount_name, discount_rate, discount_amount = None, 0.0, 0.0
+    discount_name, discount_rate, discount_amount = None, 0.0, 0.0 # set defaults
 
 total = round(subtotal - discount_amount, 2)
 
-
+print("\n----------\n")
+print("Braden's Movie Theatre Receipt\n")
+print(f"{tickets} tickets @ $10: ${subtotal:.2f}")
+if discount_amount > 0:
+    pct = int(round(discount_rate * 100))
+    print(f"{discount_name} discount ({pct}%): -${discount_amount:.2f}")
+else:
+    print("No eligible discounts: $0.00")
+print(f"\nTotal: ${total:.2f}")
