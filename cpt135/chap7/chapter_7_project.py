bf1a48316890f6694b4e3cb87e9bb5732581b61e
bradene0 <bradene0@outlook.com>
2025-10-23 09:27:28 -0500

    finished chap7 project

--- cpt135/chap7/chapter_7_project.py
+++ cpt135/chap7/chapter_7_project.py
@@ -7,3 +7,32 @@ while True:
     if len(a) == 1 and a.isalpha():
         a = a.upper()
         break
+
+while True:
+    v = input("2. Verb (past tense): ").strip().lower()
+    if v.endswith("d"):
+        break
+
+while True:
+    n = input("3. Noun (plural): ").strip().lower()
+    if n.endswith("s"):
+        break
+
+while True:
+    c = input("4. Color: ").strip().lower()
+    if c in colors:
+        break
+
+while True:
+    h_in = input("5. Holiday: ").strip().lower()
+    if h_in in holidays:
+        h = h_in.title()
+        break
+
+while True:
+    p_in = input("6. Palindrome: ").strip()
+    if p_in and p_in.casefold() == p_in.casefold()[::-1]:
+        p = p_in.upper()
+        break
+
+print(f'A spy, Agent {a}, {v} into the abandoned {n} and looked for a key. All they found was a note that said, "I\'ll be wearing a {c} shirt at the {h} party." Remember the passcode "{p}".')
